a4979ce4d4f6ddb550a935c817ee753728054b95
HassanRamos <ramoshassan42@gmail.com>
2020-12-02 10:49:20 +0300

    added payment page and other files

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import Header from './Header'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './Home'
 import Checkout from './Checkout'
+import Payment from './Payment'
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+//import Orders from './Orders'
 import Login from './Login'
+import {loadStripe} from "@stripe/stripe-js/";
+import {Elements} from "@stripe/react-stripe-js";
+//import Orders from ".Orders";
 import './App.css';
+
+const promise = loadStripe('pk_test_51HPAmjDUoecKrUEk7vQNVeJwEAwDtfp0W9WWqV35pzAYnlZhP9ETzHtGC3lgBNhq4x5pkSBHNtWlQY69LIWGayW200OkrOq7yC')
 
 function App() {
   const [{},dispatch] = useStateValue();
@@ -37,15 +44,24 @@ function App() {
     <div className="App">
    
       <Switch>
-      <Route path="/login">
+      <Route path="/orders">
+        <h1>order page</h1>
+        </Route>
+        <Route path="/login">
         <Login/>
         </Route>
       <Route path="/checkout">
          <Header/>
           <Checkout/>
         </Route>
+        <Route path="/payment">
+          <Header/>
+          <Elements stripe={promise}>
+            <Payment/>
+          </Elements>
+        </Route>
         <Route path="/">
-        <Header/>
+          <Header/>
           <Home />
         </Route>
       </Switch>
